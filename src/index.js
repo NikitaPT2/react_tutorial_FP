8ad26fd3fb602d1ec3cb4d386a931a6e0f3d6006
Nikita <nikitogka@gmail.com>
2023-01-27 13:12:42 +0200

    Starter Code

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Game from './components/game';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
